f667cee48fa00739126ee5be44c70fd71c28105a
Astm <astm@Astm-MacBook.local>
2018-05-29 09:58:53 +0200

    improved the results

--- index.php
+++ index.php
@@ -4,13 +4,22 @@
 require_once("create_thumbnail.php");
 
 $path = "images";
+$thumbnail_path = "thumbs";
 $black_list = array('.','..','.DS_Store','images');
 foreach (new DirectoryIterator($path) as $file_info) {
   $image =  $file_info->getFilename();
   if (!in_array($image, $black_list)) {
-    createThumbnail("images/".$image,"images/thumbs_".$image,270,200);
+
+    // create the thumbnail
+    $result =  createThumbnail("images/".$image,$thumbnail_path."/thumbs_".$image,270,200);
+
+    // check if created or not
+    if ($result != 1){
+      echo "<br> Failed to create : <a href='images/".$image."' target='_blank'>$image</a>";
+    }
+
   }// if
 }// foreach
 
-  Echo "<a href='".$path."' target='_blank'>Thumbnails Created</a>"
+  Echo "<br> <br> <b><a href='".$thumbnail_path."' target='_blank'>Review the created thumbnails</a></b>"
 ?>
